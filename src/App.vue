c5c6ec69c73d50ec0bff7c7b9fcc0836418fdcab
monster223223 <richdev0223@gmail.com>
2023-02-24 10:52:38 -0500

    design

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Helloworld />
+  </div>
+</template>
+
+<script>
+import Helloworld from './components/Helloworld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Helloworld
+  }
+}
+</script>
---
+++ src/components/Helloworld.vue
@@ -0,0 +1,577 @@
+<template>
+  <div>
+    <div class="bg">
+      <div class="container">
+        <div class="navbar">
+          <div class="navbar-left">
+            <img src="Group 15.png" style="padding-top:">
+            <!-- <p style="float:left;padding-right: 20px;">&#9776;</p>
+            <p style="float:left;color:#6309FF;font-size: 20px;">HUMAN</p> -->
+          </div>
+          <div class="navbar-right">
+            <a href="" style="float: right;padding-left: 20px;font-weight: 400;font-size: 16px;color:#320A8D;text-decoration: none;font-family: 'Alliance No1';">Explorer</a>
+            <a href="" style="float: right;padding-left: 20px;font-weight: 400;font-size: 16px;color:#320A8D;text-decoration: none;font-family: 'Alliance No1';">Blog</a>
+            <a href="" style="float: right;padding-left: 20px;font-weight: 400;font-size: 16px;color:#320A8D;text-decoration: none;font-family: Alliance No1;">Impact</a>
+            <a href="" style="float: right;padding-left: 20px;font-weight: 400;font-size: 16px;color:#320A8D;text-decoration: none;font-family: Alliance No1;">Resources</a>
+            <a href="" style="float: right;padding-left: 20px;font-weight: 400;font-size: 16px;color:#320A8D;text-decoration: none;font-family: Alliance No1;">Community</a>
+            <a href="" style="float: right;padding-left: 20px;font-weight: 400;font-size: 16px;color:#320A8D;text-decoration: none;font-family: Alliance No1;">Build</a>
+            <a href="" style="float: right;padding-left: 20px;font-weight: 400;font-size: 16px;color:#320A8D;text-decoration: none;font-family: Alliance No1;">Discover</a>
+          </div>
+        </div>
+        <div class="fl fl-row fl-container fl-align">
+          <div class="fl fl-column fl-align">
+             <p style="color:#6309FF;font-weight: 800;font-size: 60px;">HUMAN Token<br />(HMT)</p>
+             <p style="color:#320A8D;font-weight: 400;font-size: 14px;">Fuelling the HUMAN ecosystem</p>
+             <div class="fl fl-row">
+              <button class="learn">Learn More</button>
+             </div>
+          </div>
+          <div class="fl fl-align">
+            <img src="Group 991.png" style="width:500px;height: 400px;">
+          </div>
+        </div>
+        <div class="second">
+          <div class="movie-part">
+            <p style="position: relative;padding-left:700px;padding-top:200px;font-weight: 400;font-size: 44px;color:#320A8D;">HMT funds the processing <br />of digitized work, and <br />rewards workers for their <br />contributions.</p>
+            <img src="Group 1319.png" style="position:absolute;;left:100px;top:550px;width:608px;height: 608px;">
+          </div>
+          </div>
+          <div class="third-container" style="margin-top:250px;">
+            <div class="fl fl-container fl-column fl-align">
+              <P style="font-weight: 600;font-size: 60px;color:#320A8D;margin:20px;">Token Details</P>
+              <div class="table fl fl-row fl-container fl-align">
+                <div class="fl fl-row fl-container fl-align">
+                  <div class="fl fl-column fl-align">
+                    <div style="border-bottom:1px solid #EEEEEE">
+                    <p>Ticker</p>
+                    <p>HMT</p>
+                  </div>
+                  <div class="fl fl-column fl-align">
+                    <p>Token Price</p>
+                    <div class="fl fl-row fl-container fl-align">
+                      <p>USD</p>
+                      <p>7.6</p>
+                    </div>
+                  </div>
+                  </div>
+                </div>
+                <div class="fl fl-column fl-align" style="padding-left:20px;">
+                  <div class="fl" style="justify-content: space-around;border-bottom:1px solid #EEEEEE">
+                    <p>Circulating Supply</p>
+                    <p>120,000,000</p>
+                  </div>
+                  <div class="fl" style="justify-content: space-around;border-bottom:1px solid #EEEEEE">
+                    <p>Circulating Supply</p>
+                    <p>120,000,000</p>
+                  </div>
+                  <div class="fl" style="justify-content: space-around;border-bottom:1px solid #EEEEEE">
+                    <p>Circulating Supply</p>
+                    <p>120,000,000</p>
+                  </div>
+                </div>
+              </div>
+              <div class="fl fl-row fl-container fl-align" style="margin-top:20px;margin-bottom:20px">
+                <div class="second-line-left fl fl-row fl-container fl-align" style="padding-right:12 0px;">
+                  <p style="padding-right:50px;padding-left:10px;">Token Type</p>
+                  <p style="padding-left:50px;padding-right:10px;">ERC-20</p>
+                </div>
+                <div class="second-line-right" style="margin-left:60px;">
+                  <div class="fl fl-row fl-container fl-align">
+                    <p style="padding-left:30px;">Contract Addresses</p>
+                    <p style="padding-left:30px;">Ethereum</p>
+                    <p style="padding-left:30px;padding-right:30px;">polygon</p>
+                  </div>
+                </div>
+              </div>
+              <div>
+                <img src="Group 1283.png" style="width:670px;margin-bottom: 20px;">
+              </div>
+            </div>
+          </div>
+          <div class="third" style="margin-top:70px;">
+              <div class="fl fl-column fl-container fl-align">
+                <p class="HMT">HUMAN in the media</p>
+                <div class="fl fl-wrap">
+                  <img src="Group 1286.png" style="width:25%">
+                  <img src="Group 1286.png" style="width:25%">
+                  <img src="Group 1286.png" style="width:25%">
+                  <img src="Group 1286.png" style="width:25%">
+                  <img src="Group 1286.png" style="width:25%">
+                  <img src="Group 1286.png" style="width:25%">
+                  <img src="Group 1286.png" style="width:25%">
+                  <img src="Group 1286.png" style="width:25%">
+                </div>
+              </div>
+            </div>
+            <div class="circular-progressbar">
+              <img src="Group 1300.png">
+            </div>
+            <div class="graph-show">
+              <img src="Group 1298.png">
+            </div>
+            <div class="fourth" style="margin-top:70px;">
+              <div class="fl fl-column fl-container fl-align">
+                <p class="HMT">How do I get HMT?</p>
+                <div class="fl fl-row fl-container fl-align">
+                  <div class="fourth-elem fl-column fl fl-align">
+                    <img src="Group 601.png" style="width:53px;height:74px">
+                    <p>Complete Jobs</p>
+                  </div>
+                  <div class="fourth-elem fl-column fl fl-align">
+                    <img src="Group 601.png" style="width:53px;height:74px">
+                    <p>Complete Jobs</p>
+                  </div>
+                  <div class="fourth-elem fl-column fl fl-align">
+                    <img src="Group 601.png" style="width:53px;height:74px">
+                    <p>Complete Jobs</p>
+                  </div>
+                  <div class="fourth-elem fl-column fl fl-align">
+                    <img src="Group 601.png" style="width:53px;height:74px">
+                    <p>Complete Jobs</p>
+                  </div>
+                  <div class="fourth-elem fl-column fl fl-align">
+                    <img src="Group 601.png" style="width:53px;height:74px">
+                    <p>Complete Jobs</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="fifth" style="margin-top:70px;">
+              <div class="fl fl-column fl-container fl-align">
+                <p class="HMT">Utility of the HUMAN Token</p>
+                <div class="fl fl-row fl-container fl-align">
+                  <div class="fourth-elem fl-column fl fl-align">
+                    <img src="Group 439.png" style="width:53px;height:74px">
+                    <p>Job Requeseters</p>
+                  </div>
+                  <div class="fourth-elem fl-column fl fl-align">
+                    <img src="Group 601.png" style="width:53px;height:74px">
+                    <p>Complete Jobs</p>
+                  </div>
+                  <div class="fourth-elem fl-column fl fl-align">
+                    <img src="Group 601.png" style="width:53px;height:74px">
+                    <p>Complete Jobs</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="sixth">
+              <p class="disclaimer">Disclaimer</p>
+              <p class="disclaimer-content">The HUMAN Protocol Foundation makes no representation, warranty, or undertaking, express or implied, as to the accuracy, reliability, <br />completeness, or reasonableness of the Information. Any assumptions, opinions, and estimates expressed in the information on this page <br />
+                constitutes the HUMAN Protocol Foundation’s judgment as of 18/11/21 and are subject to change without notice. The HUMAN Protocol <br />
+                Foundation does not accept any liability for any direct, consequential or other loss arising from reliance on the contents of the information <br />
+                in this graphic.</p>
+            </div>
+            <div class="seventh">
+              <p class="entry">Recent Entries</p>
+              <div class="fl fl-row fl-container fl-align">
+              <div class="entry-content fl fl-column fl-align" style="margin-right:60px;">
+                <img src="Mask group.png" style="width:450px;height:386px;">
+                <p>Mar 31 2022</p>
+                <p>HUMAN Protocol to launch new <br />Reputation Oracles powered by<br /> Chainlink</p>
+                <button>Community</button>
+              </div>
+              <div class="entry-content fl fl-column fl-align" style="margin-right:60px;">
+                <img src="Mask group (1).png" style="width:450px;height:386px;">
+                <p>Mar 31 2022</p>
+                <p>HUMAN Protocol to launch new <br />Reputation Oracles powered by<br /> Chainlink</p>
+                <button>Community</button>
+              </div>
+              </div>
+              <div class="fl fl-row Lfloat">
+                <img src="arrow (1).png">
+                <img src="arrow.png">
+              </div>
+            </div>
+            <div class="footer-up">
+              <div class="fl fl-row fl-align space-between">
+                <div class="footer-up-left">
+                  <p class="footer-up-name">Get HUMAN news and updates</p>
+                  <div class="fl fl-row">
+                    <input class="input-style" style="margin-right:30px;">
+                    <button class="submit">Subscribe</button>
+                  </div>
+                </div>
+                <div class="footer-up-right">
+                  <div class="fl space-between">
+                    <img src="Combined Shape.png" style="padding-right:30px;">
+                    <img src="Vector.png" style="padding-right:30px;">
+                    <img src="Fill 1 (1).png" style="padding-right:30px;">
+                    <img src="Combined Shape (1).png" style="padding-right:30px;">
+                    <img src="Frame.png" style="padding-right:30px;">
+                  </div>
+                </div>
+              </div>
+            </div>
+      </div>
+      <div class="footer-middle">
+        <div class="fl space-around">
+          <div>
+            <p>Products</p>
+            <p>HUMAN APP</p>
+            <p>Proof of Humanity</p>
+            <p>IMOO</p>
+          </div>
+          <div>
+            <p>Discover</p>
+            <p>Token</p>
+            <p>Routing Layer</p>
+            <p>Team</p>
+            <p>Careers</p>
+          </div>
+          <div>
+            <p>Build</p>
+            <p>Developers</p>
+            <p>Technology</p>
+            <p>DevBounty Program</p>
+            <p>BugBounty Program</p>
+            <p>Github</p>
+            <p>Grants Program</p>
+          </div>
+        </div>
+      </div>
+      <div class="footer-down">
+        <div class="footer-down-container">
+        <p class="footer-down-title">Third-party communities</p>
+        <div class="fl space-between">
+          <div class="fl fl-column">
+            <div class="fl fl-row">
+              <img src="logo.png" style="width:31px;height: 24px;">
+              <p>Telegram</p>
+            </div>
+            <input class="Tel-input">
+            <div class="fl space-between" style="margin-top:30px;margin-bottom:30px;">
+              <img src="Group 345.png" style="width:95px;heigh:44px">
+              <img src="Group 870.png" style="width:193px;heigh:44px">
+              <img src="Group 356.png" style="width:99px;heigh:44px">
+              <img src="Group 347.png" style="width:99px;heigh:44px">
+              <img src="Group 348.png" style="width:89px;heigh:44px">
+            </div>
+          </div>
+          <div class="fl fl-column">
+            <div class="fl fl-row">
+              <img src="Group 338.png" style="width:31px;height: 24px;">
+              <p>Mixin</p>
+            </div>
+            <input class="Mixin-input">
+            <div class="fl space-between" style="margin-top:30px;margin-bottom:30px;">
+              <img src="Group 350.png" style="width:154px;heigh:44px">
+            </div>
+          </div>
+        </div>
+        <div class="fl space-between" style="margin-top:100px;margin-bottom: 40px;">
+          <p class="mark">© 2021 HPF. HUMAN Protocol® is a registered trademark</p>
+          <!-- <div class="fl space-around">
+            <div class="link-1">
+              <p>contact@hmt.ai</p>
+            </div>
+            <div class="link-1">
+              <p>Terms and Conditions</p>
+            </div>
+          </div> -->
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.learn{
+  background: linear-gradient(145.2deg, #F0F2FC -4.32%, #FFFFFF 61.97%);
+/* White */
+
+border: 4px solid #FFFFFF;
+border-radius: 10px;
+}
+.HMT{
+  font-family: 'Alliance No.1';
+font-style: normal;
+font-weight: 600;
+font-size: 60px;
+line-height: 72px;
+/* identical to box height, or 120% */
+
+letter-spacing: -0.4px;
+
+/* Blue/400 */
+
+color: #320A8D;
+}
+.link-1{
+  background-color: white;
+  border:1px solid white;
+  border-radius: 20px;
+}
+.mark{
+  font-family: 'Alliance No1';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 24px;
+/* or 171% */
+
+letter-spacing: 0.2px;
+
+/* Blue/400 */
+
+color: #320A8D;
+}
+.Tel-input{
+  width:100%;
+  border:none;
+  border-bottom:1px solid #320A8D;
+  background-color: #aaacae;
+}
+.Mixin-input{
+  width:100%;
+  border:none;
+  border-bottom:1px solid #320A8D;
+  background-color: #aaacae;
+}
+.footer-down-container{
+  padding-right: 80px;
+  padding-left: 80px;
+}
+.footer-down-title{
+  margin:0;
+  font-family: 'Alliance No.1';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 24px;
+color: #320A8D;
+padding-top:60px;
+padding-bottom: 60px;
+}
+.footer-down{
+  background-color: #aaacae;
+}
+.footer-middle{
+  background-color:#F2F2FA;
+}
+.space-around{
+  justify-content: space-around;
+}
+.space-between{
+  justify-content: space-between;
+}
+.footer-up{
+  clear: right;
+  margin-bottom:50px;
+}
+.input-style{
+  border:1px solid #320A8D;
+  border-radius: 5px;
+}
+.submit{
+  background: #320A8D;
+border-radius: 10px;
+text-align: center;
+color:white;
+font-family: 'Alliance No.1';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 24px;
+}
+.footer-up-name{
+  font-family: 'Alliance No.1';
+font-style: normal;
+font-weight: 600;
+font-size: 24px;
+line-height: 28px;
+/* identical to box height, or 117% */
+
+letter-spacing: 0.32px;
+
+/* Blue/400 */
+
+color: #320A8D;
+}
+.Lfloat{
+  float:right;
+}
+.entry-content{
+  background-color: #F2F2FA;
+  border:1px solid #F2F2FA;
+  border-radius: 10px;
+}
+.entry{
+  font-family: 'Alliance No.1';
+font-style: normal;
+font-weight: 600;
+font-size: 60px;
+line-height: 72px;
+/* or 120% */
+
+letter-spacing: -0.4px;
+
+/* Blue/400 */
+
+color: #320A8D;
+}
+.sixth{
+  margin:90px 80px;
+}
+.disclaimer{
+  font-family: 'Alliance No1';
+font-style: normal;
+font-weight: 400;
+font-size: 48px;
+line-height: 56px;
+/* identical to box height, or 117% */
+
+letter-spacing: 0.15px;
+
+/* Blue/400 */
+
+color: #320A8D;
+}
+.disclaimer-content{
+  font-family: 'Alliance No1';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+line-height: 28px;
+/* or 156% */
+
+letter-spacing: 0.1px;
+
+/* Blue/400 */
+
+color: #320A8D;
+}
+.fifth{
+  background: #F2F2FA;
+  border:1px solid #F2F2FA;
+  border-radius: 40px;
+  width:100%;
+}
+.fourth{
+  background: #F2F2FA;
+  border:1px solid #F2F2FA;
+  border-radius: 40px;
+  width:100%;
+}
+.fourth-elem{
+  background-color: white;
+  margin:0px 20px 20px 50px;
+  padding:20px 20px;
+}
+.circular-progressbar{
+  margin: 200px 0px;
+}
+.graph-show{
+  margin:200px 0px;
+}
+.third{
+  background: #F2F2FA;
+  border:1px solid #F2F2FA;
+  border-radius: 40px;
+  width:100%;
+}
+.fl-wrap{
+  flex-wrap: wrap;
+}
+.second-line-left{
+  background-color: white;
+  border:1px solid white;
+  border-radius: 26px;
+}
+.second-line-right{
+  background-color: white;
+  border:1px solid black;
+  border-radius: 26px;
+}
+.third-container{
+  margin-left: -90px;
+  margin-right:-90px;
+  background: linear-gradient(167.14deg, #F0F2FC 9.58%, #FFFFFF 93.46%);
+}
+.table{
+  background-color: white;
+  width:668px;
+  height:220px;
+  border:1px solid white;
+  border-radius: 15px;
+}
+.titles{
+  width:100%;
+}
+.fl{
+  display: flex;
+}
+.fl-row{
+  flex-direction: row;
+}
+.fl-column{
+  flex-direction: column;
+}
+.fl-container{
+  justify-content: center;
+}
+.fl-align{
+  align-items: center;
+}
+ .bg{
+  background-color: white;
+ }
+ .container{
+  margin-right: auto;
+  margin-left:auto;
+  padding-right:80px;
+  padding-left: 80px;
+ }
+ .navbar{
+  display: flex;
+  justify-content: space-between;
+  padding-top:20px;
+ }
+ 
+ .navbar-right{
+
+ }
+</style>
+<script>
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'Helloworld',
+  data() {
+    return {
+      spellName: null
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'spells',
+      'singleSpell',
+      'base',
+      'key'
+    ]),
+  },
+  mounted() {
+    this.fetchSpells()
+  },
+  methods: {
+    ...mapActions([
+      'fillSpells',
+      'fillSingleSpell'
+    ]),
+    fetchSpells() {
+      const base = this.base
+      const key = this.key
+      fetch(`${base}/spells?key=${key}`)
+      .then(resp => resp.json())
+      .then(obj => {
+        this.fillSpells(obj)
+      })
+    },
+    search() {
+      const singleSpell = this.spells.filter(spell => spell.spell.toLowerCase().includes(this.spellName.toLowerCase()))
+      this.fillSingleSpell(singleSpell)
+    }
+  }
+}
+</script>
